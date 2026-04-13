c81a35ccec9b4cd7a10dafaa097a11444f29635b
HelloWorld <sarojcrest45@gmail.com>
2026-04-13 17:13:16 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (80.10%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,21 +1,46 @@
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         int []square= new int[nums.length];
+//         for(int i=0;i<nums.length;i++){
+//             square[i]=nums[i]*nums[i];
+//         }
+//         int temp;
+//         for(int i=0;i<square.length;i++){
+//             for(int j=i+1;j<square.length;j++){
+//             if(square[i]>square[j]){
+//                 temp=square[i];
+//                 square[i]=square[j];
+//                 square[j]=temp;
+//             }
+//             }
+//         }
+//         return square;
+//     }
+// } // here its time complexity is O(n2) so we gonna use two pointer instead
+
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-
-        int left = 0, right = n - 1;
-        int k = n - 1;
-
-        while (left <= right) {
-            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
-                result[k--] = nums[left] * nums[left];
-                left++;
-            } else {
-                result[k--] = nums[right] * nums[right];
-                right--;
-            }
+    int n=nums.length;
+    int[]result =new int[n];
+    int left=0; // pointer from starting
+    int right=n-1; // pointer from ending
+    int k=n-1; // to put the greatest square from last position so as to maintain ascending order
+    while(left<=right){
+        int left_square=nums[left]*nums[left];
+        int right_square=nums[right]*nums[right];
+        if(left_square>right_square){
+            result[k]=left_square;
+            left++;
         }
-
-        return result;
+        else{
+            result[k]=right_square;
+            right--;
+        }
+        k--;
     }
+    return result;
+
+    }
+
 }
