3ef5c602f19080e8aeb031632dc4cb0ad9404c3b
HelloWorld <sarojcrest45@gmail.com>
2026-04-30 03:55:11 +0530

    Time: 55 ms (5.28%) | Memory: 42.4 MB (89.26%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,24 +1,35 @@
+// class Solution {
+//     public int mySqrt(int x) {
+//         if (x == 0) return 0;  
+        
+//         int low = 1;
+//         int high = x;
+        
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+            
+//             if (mid == x / mid) {
+//                 return mid;
+//             } 
+//             else if (mid < x / mid) {
+//                 low = mid + 1;
+//             } 
+//             else {
+//                 high = mid - 1;
+//             }
+//         }
+        
+//         return high;
+//     }
+// }
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0) return 0;  // 🔥 fix division by zero
-        
-        int low = 1;
-        int high = x;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (mid == x / mid) {
-                return mid;
-            } 
-            else if (mid < x / mid) {
-                low = mid + 1;
-            } 
-            else {
-                high = mid - 1;
-            }
+        int root = 0;
+
+        for (int i = 1; i <= x / i; i++) {
+            root = i;
         }
-        
-        return high;
+
+        return root;
     }
 }
