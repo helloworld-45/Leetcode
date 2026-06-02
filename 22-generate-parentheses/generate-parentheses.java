6f5bbe4461d52ed7d4faffa440ce6133e1955d04
HelloWorld <sarojcrest45@gmail.com>
2026-06-02 23:51:48 +0530

    Time: 2 ms (71.81%) | Memory: 44.8 MB (45.32%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current,
+                           int open, int close, int n) {
+
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+}
+// Backtracking:
+// Add '(' if open brackets used are less than n.
+// Add ')' only when close brackets are fewer than open brackets.
+// When the string length reaches 2*n, add it to the answer.
