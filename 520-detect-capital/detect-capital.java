b8c42eeee77fe26fe82dceab300aca930421ec6e
HelloWorld <sarojcrest45@gmail.com>
2026-06-11 00:13:36 +0530

    Time: 1 ms (88.67%) | Memory: 43.1 MB (63.67%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        int count = 0;
-        for(int i = 0; i < word.length(); i++) {
-            if(Character.isUpperCase(word.charAt(i)))
-                count++;
+        int count =0;
+        for(int i=0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                count ++;
+            }
         }
-        if(count == word.length()) // all uppercase
-            return true;
-        if(count == 0) // all lowercase
-            return true;
-        if(count == 1 && Character.isUpperCase(word.charAt(0)))
-            return true;
-        return false;
+        if(count == word.length()) return true;
+        else if (count == 0) return true;
+        else if (count ==1 && Character.isUpperCase(word.charAt(0))) return true;
+        else return false;
     }
 }
