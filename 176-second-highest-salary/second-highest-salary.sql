92c4adff49cbdeee9782a169c73de34db4e9298d
HelloWorld <sarojcrest45@gmail.com>
2026-06-12 00:02:23 +0530

    Time: 264 ms (91.64%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,6 @@
-SELECT (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+select(
+    select distinct salary
+    from Employee
+    order by salary desc
+    limit 1 offset 1
+) as SecondHighestSalary
