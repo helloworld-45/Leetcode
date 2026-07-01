1ff0db6e4895dbf647cf745d75913ec16f71fc9b
HelloWorld <sarojcrest45@gmail.com>
2026-07-01 13:20:03 +0530

    Time: 1 ms (99.82%) | Memory: 47.6 MB (87.76%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,16 @@
-class Solution{
-
-    public void moveZeroes(int[] nums){
+class Solution {
+    public void moveZeroes(int[] nums) {
         int n=nums.length;
-        int k=0;
+        int j=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[k++]=nums[i];
+                nums[j]=nums[i];
+                j++;
             }
         }
-        while(k<n){
-            nums[k++]=0;
+        while(j<n){
+            nums[j]=0;
+            j++;
         }
-
     }
 }
