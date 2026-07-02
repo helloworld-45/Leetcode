65138db2fbaaf7c0d4a07b984fc724dcfd7139bf
HelloWorld <sarojcrest45@gmail.com>
2026-07-02 23:20:51 +0530

    Time: 14 ms (48.05%) | Memory: 46.2 MB (79.04%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        int left=0;
+        int count=0;
+        int max=0;
+        for(int right=0;right<s.length();right++){
+            if(isvowel(s.charAt(right)))count++;
+            if((right -left +1)> k){
+                if(isvowel(s.charAt(left))){
+                    count--;
+                }
+                left++;
+            }
+            if((right-left+1)==k){
+                max=Math.max(max,count);
+            }
+        }
+        return max;
+    }
+    public boolean isvowel(char ch){
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+    }
+}
+/*
+ * Sliding Window:
+    Instead of counting vowels for every substring, maintain the vowel count
+    of the current window. As the window moves, add the new character if it's
+    a vowel and remove the leftmost character if it leaves the window. Update
+    the maximum vowel count whenever the window size becomes k.
+ */
