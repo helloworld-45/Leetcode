2838b345f80752752a770532f01de8540a3af9d1
HelloWorld <sarojcrest45@gmail.com>
2026-07-05 17:34:43 +0530

    Time: 17 ms (15.91%) | Memory: 52.7 MB (76.51%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n=nums.length;
-        Arrays.sort(nums);
-        return nums[n/2];
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for(int el:nums){
+            map.put(el,map.getOrDefault(el,0)+1);
+            if(map.get(el)>n/2)return el;
+        }
+        return -1;
     }
 }
+//using Hashmap technique whose time complexity is O(log(n))
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         Arrays.sort(nums);
+//         return nums[n/2];
+//     }
+// }
+// using sorting technique, time complexity is O(nlogn)
