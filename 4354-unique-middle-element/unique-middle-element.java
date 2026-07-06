c38cc9d3c5e18fc5b48dc69004d86cecca702db3
HelloWorld <sarojcrest45@gmail.com>
2026-07-06 19:51:52 +0530

    Time: 4 ms (100.00%) | Memory: 46.2 MB (100.00%) - LeetSync

---
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+       int n=nums.length;
+       HashMap<Integer,Integer> map= new HashMap<>();
+       for(int el:nums){
+        map.put(el,map.getOrDefault(el,0)+1);
+       } 
+       return map.get(nums[n/2])==1;
+    }
+}
+// Count the frequency of each element using a HashMap.
+// If the middle element appears only once, return true; otherwise, return false.
+// time: O(n) , space=O(n)
+
+
+// class Solution {
+//     public boolean isMiddleElementUnique(int[] nums) {
+//         int middle = nums[nums.length / 2];
+//         int count = 0;
+//         for (int num : nums) {
+//             if (num == middle) {
+//                 count++;
+//             }
+//         }
+//         return count == 1;
+//     }
+// }
+//brute for technique
+// time: O(n) , space=O(1)
