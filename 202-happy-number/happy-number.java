764a608cd42b7fe78974f14168208f4f5714f3ed
HelloWorld <sarojcrest45@gmail.com>
2026-07-10 18:32:42 +0530

    Time: 2 ms (15.49%) | Memory: 42.6 MB (34.18%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-
-        do {
-            slow = squareSum(slow);
-            fast = squareSum(squareSum(fast));
-        } while (slow != fast);
-
-        return slow == 1;
-    }
-
-    private int squareSum(int n) {
-        int sum = 0;
-
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+        HashSet<Integer> set= new HashSet<>();
+        while(n!=1){
+            if(set.contains(n))return false;
+            set.add(n);
+            n=squaresum(n);
         }
-
+        return true;
+    }
+    public int squaresum(int n){
+        int sum=0;
+        while(n!=0){
+            int r=n%10;
+            sum+=r*r;
+            n/=10;
+        }
         return sum;
     }
 }
-// Use Floyd Cycle Detection (slow & fast pointers)
-// If number becomes 1 -> happy number
-// If cycle forms -> not happy
+// Use a HashSet to keep track of the numbers we've already visited.
+// If we reach 1, the number is happy. If a number repeats, it means
+// we've entered a cycle, so the number cannot become 1. In each step,
+// calculate the sum of the squares of its digits and continue the process.
