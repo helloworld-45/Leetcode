75bd20b3683094f9b19b48b7b9480819f8e5bceb
HelloWorld <sarojcrest45@gmail.com>
2026-07-10 17:36:02 +0530

    Time: 2 ms (100.00%) | Memory: 46.3 MB (91.55%) - LeetSync

--- 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.java
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxSum(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int []freq= new int[10];
         int ans=-1;
         for(int num:nums){
-            int maxnum=maxDigit(num);
-            if(map.containsKey(maxnum)){
-                ans=Math.max(ans,map.get(maxnum)+num);
-                map.put(maxnum,Math.max(map.get(maxnum),num));
+            int getmax=maxDigit(num);
+            if(freq[getmax]!=0){
+                ans=Math.max(ans,freq[getmax]+num);
             }
-            else map.put(maxnum,num);
+            freq[getmax]=Math.max(freq[getmax],num);
         }
         return ans;
-        
     }
     public int maxDigit(int n){
         int max=0;
@@ -22,3 +20,34 @@ class Solution {
         return max;
     }
 }
+// Store the largest number for each maximum digit (0-9) in an array.
+// If another number has the same maximum digit, update the maximum pair sum
+// and keep only the larger number for future comparisons.
+
+// class Solution {
+//     public int maxSum(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int ans = -1;
+//         for (int num : nums) {
+//             int maxDigit = getMaxDigit(num);
+//             if (map.containsKey(maxDigit)) {
+//                 ans = Math.max(ans, map.get(maxDigit) + num);
+//                 map.put(maxDigit, Math.max(map.get(maxDigit), num));
+//             } else {
+//                 map.put(maxDigit, num);
+//             }
+//         }
+//         return ans;
+//     }
+//     private int getMaxDigit(int num) {
+//         int max = 0;
+//         while (num > 0) {
+//             max = Math.max(max, num % 10);
+//             num /= 10;
+//         }
+//         return max;
+//     }
+// }
+// Store the largest number for each maximum digit (0-9).
+// When another number has the same maximum digit, calculate their sum
+// and update the answer. Keep only the larger number for future pairs.
