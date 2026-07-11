717e1f1723614abaeb7a648a29310cf510d5a44e
HelloWorld <sarojcrest45@gmail.com>
2026-07-11 14:46:31 +0530

    Time: 2 ms (97.78%) | Memory: 45.3 MB (12.93%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,27 +1,57 @@
-import java.util.ArrayList;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        ArrayList<Integer> ans=new ArrayList<>();
+        HashSet<Integer> set= new HashSet<>();
+        for(int num:nums1){
+            set.add(num);
+        }
+        HashSet<Integer> result= new HashSet<>();
+        for(int num:nums2){
+            if(set.contains(num))result.add(num);
+        }
+        int []ans= new int[result.size()];
         int i=0;
-        int j=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]==nums2[j]){
-                if(ans.isEmpty() ||ans.get(ans.size()-1)!=nums1[i]){
-                ans.add(nums1[i]);
-                }
-                j++;
-                i++;
-            }
-            else if(nums1[i]<nums2[j]) i++;
-            else j++; 
+        for(int el:result){
+            ans[i++]=el;
         }
-        int[]result=new int[ans.size()];
-        for(int k=0; k< ans.size();k++){
-            result[k]=ans.get(k);
-        }
-        return result;
-
+        return ans;
     }
 }
+/*
+Approach:
+1. Store all elements of nums1 in a HashSet.
+2. Traverse nums2.
+3. If an element exists in the HashSet, add it to the result HashSet.
+4. Result HashSet automatically removes duplicates.
+5. Convert the result set to an array.
+Time Complexity: O(n + m)
+Space Complexity: O(n)
+*/
+
+
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         ArrayList<Integer> ans=new ArrayList<>();
+//         int i=0;
+//         int j=0;
+//         while(i<nums1.length && j<nums2.length){
+//             if(nums1[i]==nums2[j]){
+//                 if(ans.isEmpty() ||ans.get(ans.size()-1)!=nums1[i]){
+//                 ans.add(nums1[i]);
+//                 }
+//                 j++;
+//                 i++;
+//             }
+//             else if(nums1[i]<nums2[j]) i++;
+//             else j++; 
+//         }
+//         int[]result=new int[ans.size()];
+//         for(int k=0; k< ans.size();k++){
+//             result[k]=ans.get(k);
+//         }
+//         return result;
+
+//     }
+// }
