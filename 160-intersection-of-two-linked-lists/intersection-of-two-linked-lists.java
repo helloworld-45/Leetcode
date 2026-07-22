ea95aa7bd5c8cdf5af5493c7f994ab6d288ad0b9
HelloWorld <sarojcrest45@gmail.com>
2026-07-22 14:41:36 +0530

    Time: 1 ms (99.93%) | Memory: 52.8 MB (45.26%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,15 +11,30 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> ans= new HashSet<>();
-        while(headA!=null){
-            ans.add(headA);
-            headA=headA.next;
-        }
-        while(headB!=null){
-            if(ans.contains(headB))return headB;
-            headB=headB.next;
-        }
-        return null;
+    ListNode p1=headA;
+    ListNode p2=headB;
+    while(p1!=p2){
+        p1=(p1==null)?headB:p1.next;
+        p2=(p2==null)?headA:p2.next;
+    }
+    return p2;
     }
 }
+//i used two pointer approach, here i have used p1 and p2 to perform traversing the node if both of them cover the same distance and they reach at the same point which is not null then that is the required intersection point otherwise if it reaches null it means there is no any intersection point
+
+
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         HashSet<ListNode> ans= new HashSet<>();
+//         while(headA!=null){
+//             ans.add(headA);
+//             headA=headA.next;
+//         }
+//         while(headB!=null){
+//             if(ans.contains(headB))return headB;
+//             headB=headB.next;
+//         }
+//         return null;
+//     }
+// }
